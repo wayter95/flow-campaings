56592dfff05bf99f7794b5fa0a7d04571e4efcce
Wayter Paulo <wayter.paulo.95@gmail.com>
2026-03-15 00:20:04 -0300

    feat: update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,10 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function proxy(request: NextRequest) {
-  const token = await getToken({ req: request });
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET,
+  });
 
   if (!token) {
     const loginUrl = new URL("/login", request.url);
